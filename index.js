12ce2918b189c75f0b3cc6830e4fb96ef44e516a
Godswillomuka <omukagodswil@gmail.com>
2024-10-08 09:01:48 +0300

    index.js

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  const apiUrl = 'https://anapioficeandfire.com/api/books';
+   return fetch(apiUrl)
+   .then(response => {
+    if (!response.ok) {
+      throw new console.error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    renderBooks(data);
+})
+.catch(error => {
+  console.error('There has been a problem with your fetch operation', error);
+});
   
 }
 
